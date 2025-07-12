4a502740e54b874f04ed98ce1801aac7af1bb31a
Roland Szucs <rolandszucs.private@gmail.com>
2025-07-12 19:54:59 +0200

    mobile css and ts done

--- my-project/src/main.ts
+++ my-project/src/main.ts
@@ -1,1 +1,13 @@
+const hamburgerButton = document.getElementById('hamburger-icon')!;
+const mobileMenu = document.getElementById('small-screen-nav-container')!;
+const overlay = document.getElementById('overlay')!;
+const body = document.body;
 
+hamburgerButton.addEventListener('click', () => {
+  const menuIsOpen = hamburgerButton.classList.toggle('menu-open');
+  mobileMenu.classList.toggle('open');
+  overlay.classList.toggle('active');
+
+  // Disable or enable scrolling
+  body.classList.toggle('no-scroll', menuIsOpen);
+});
